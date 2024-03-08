4145d47d7e7f9a5bc35c3f2449de814ecce72339
yashdave727 <yashdave727@gmail.com>
2024-03-08 16:28:39 -0500

    Fixed an error in the naming of the container for the docker in runme_db.sh

--- runme_db.sh
+++ runme_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t assignmentpostgres .
-docker run -d --name mypostgrescontainer -p 5432:5432 mypostgres
+docker run -d --name assignmentpostgrescontainer -p 5432:5432 assignmentpostgres
