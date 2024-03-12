748e7f4cd7ac32a0eb73d4e3ecd72dd160ac60cb
Bennet Dela Rosa <105404912+userBD10@users.noreply.github.com>
2024-03-11 21:08:03 -0400

    4 setup database interactions class (#20)
    * Reverted back the tests dir to the original one, and removed unnecessary files
    
    * Removed the comments in csc301_a2/runme.sh file
    
    ---------
    
    Co-authored-by: Mars Ponce <marcelo.ponce.ardon@gmail.com>

--- src/OrderService/OrderService.java
+++ src/OrderService/OrderService.java
@@ -124,6 +124,7 @@ public class OrderService
                     String response = jsonObject.toString();
                     sendResponse(exchange, statusCode, response);
                 }
+            
             }
             catch (Exception e)
             {
