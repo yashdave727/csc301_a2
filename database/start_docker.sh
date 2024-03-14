82a12aa45e4a3e3b9b6acada9db4be657323dfc2
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2024-03-14 16:02:57 -0400

    Added check that we're running on VM before executing

--- database/start_docker.sh
+++ database/start_docker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Check that we're running on the school VM
+# We can check the ip using curl ipinfo.io/ip
+# It must equal 142.1.44.57
+if [ $(curl -s ipinfo.io/ip) != "142.1.44.57" ]; then
+    echo "You are not running on the school VM"
+    exit 1
+fi
+
 # First, check that containers are not running
 # If they are, tell user to run stop_docker.sh
 if [ "$(docker ps -q -f name=assignmentpostgrescontainer)" ]; then
