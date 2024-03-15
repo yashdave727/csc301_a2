198583ec7029aa3209c21989937aff4fb551698f
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2024-03-15 18:18:04 -0400

    fix connect_to_db.sh

--- database/connect_to_db.sh
+++ database/connect_to_db.sh
@@ -3,5 +3,11 @@
 # Read in the port from the command line
 port=$1
 
+# if no port is given, exit
+if [ -z $port ]; then
+    echo "Please provide a port number"
+    exit 1
+fi
+
 echo "The password is assignmentpassword"
 psql -h 142.1.44.57 -p $port -U assignmentuser -d assignmentdb
