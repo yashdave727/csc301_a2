4fc4aed771371756f64ab275bd536fd519c4a4cd
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2024-03-15 18:17:26 -0400

    connect_to_db fix

--- database/connect_to_db.sh
+++ database/connect_to_db.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Read in the port from the command line
+port=$1
+
 echo "The password is assignmentpassword"
-psql -h 142.1.44.57 -p 5432 -U assignmentuser -d assignmentdb
+psql -h 142.1.44.57 -p $port -U assignmentuser -d assignmentdb
